4b70f92dcbe971c8d2a023f82d225a6c74751ef3
Duncan Jones <dj223@outlook.com>
2024-11-07 08:43:19 +0000

    Updated names of kernel module to donn..

--- source/donn-eitean.c
+++ source/donn-eitean.c
@@ -5,39 +5,38 @@
 #include <linux/cdev.h> 
 
 
-static dev_t hello_dev;
-struct cdev hello_cdev;
+static dev_t donn_dev;
+struct cdev donn_cdev;
 static char buffer[64];
 
-struct file_operations hello_fops = {
+struct file_operations donn_fops = {
     .owner = THIS_MODULE,
- 
 };
 
-static int __init hello_module_init(void)
+static int __init donn_module_init(void)
 {
-    printk(KERN_INFO "Loading HelloWorld_module.\n");
+    printk(KERN_INFO "Loading Donn Eitean module.\n");
 
-    alloc_chrdev_region(&hello_dev, 0, 1, "hello_dev");
-    printk(KERN_INFO "%s\n", format_dev_t(buffer, hello_dev));
+    alloc_chrdev_region(&donn_dev, 0, 1, "donn_dev");
+    printk(KERN_INFO "%s\n", format_dev_t(buffer, donn_dev));
 
-    cdev_init(&hello_cdev, &hello_fops);
-    hello_cdev.owner = THIS_MODULE;
-    cdev_add(&hello_cdev, hello_dev, 1);
+    cdev_init(&donn_cdev, &donn_fops);
+    donn_cdev.owner = THIS_MODULE;
+    cdev_add(&donn_cdev, donn_dev, 1);
 
     return 0;
 }
 
-static void __exit hello_module_cleanup(void)
+static void __exit donn_module_cleanup(void)
 {
-    printk(KERN_INFO "Cleaning-up hello_dev.\n");
-    cdev_del(&hello_cdev);
-    unregister_chrdev_region(hello_dev, 1);
+    printk(KERN_INFO "Cleaning-up donn_cdev.\n");
+    cdev_del(&donn_cdev);
+    unregister_chrdev_region(donn_dev, 1);
 }
 
-module_init(hello_module_init);
-module_exit(hello_module_cleanup);
+module_init(donn_module_init);
+module_exit(donn_module_cleanup);
 
-MODULE_AUTHOR("Your Name");
+MODULE_AUTHOR("Duncan");
 MODULE_LICENSE("GPL");
 
